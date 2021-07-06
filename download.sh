74b911bf9c1a74f59d75d6ffa18f7e08420316e5
huangjuite <huangjuite@gmail.com>
2021-07-07 00:37:32 +0800

    script

--- download.sh
+++ download.sh
@@ -2,6 +2,9 @@
 echo -e "\e[93m download model model_0706_4.pth \e[0m"
 gdown --id 1HqK539H3izCILwrwOfaUSpNcRob1g9Mb
 
+echo -e "\e[93m download model model_0706_8.pth \e[0m"
+gdown --id 1SCZp-khnkYhGoRI5qI-6DS5i5l1OfRqk
+
 echo -e "\e[93m download datasets Loggger05_8.hdf5 \e[0m"
 gdown --id 1UrjmRLTVOrWqseT578wWfqdTZbK4Cckw
 
